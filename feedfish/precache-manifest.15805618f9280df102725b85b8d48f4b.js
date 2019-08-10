bfd008e0db59b452b89d88b93687fa5546bf2f32
Star <450419074@qq.com>
2019-08-10 18:49:13 +0800

    1

---
+++ feedfish/precache-manifest.15805618f9280df102725b85b8d48f4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5213f1275d83443775b8f9eaf494311a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d54cd7e41d337fac2809",
+    "url": "/static/css/2.ff29e5f6.chunk.css"
+  },
+  {
+    "revision": "15361458acfae63e1f9a",
+    "url": "/static/css/main.7fbd6ac6.chunk.css"
+  },
+  {
+    "revision": "d54cd7e41d337fac2809",
+    "url": "/static/js/2.a6f35860.chunk.js"
+  },
+  {
+    "revision": "15361458acfae63e1f9a",
+    "url": "/static/js/main.b9d499fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
