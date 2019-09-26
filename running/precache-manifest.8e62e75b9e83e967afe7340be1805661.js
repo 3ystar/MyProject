0d8f692f64aac96b07530a4cf3781f0c1245e7c6
Star <450419074@qq.com>
2019-09-26 08:18:00 +0800

    running

---
+++ running/precache-manifest.8e62e75b9e83e967afe7340be1805661.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "79f3cb15eb62521585039c7a1e38c2cc",
+    "url": "./assets/images/icon-mission.png"
+  },
+  {
+    "revision": "6cc62ceedbe5f44b42e9",
+    "url": "./js/bundle.js"
+  },
+  {
+    "revision": "fab5b609aa0d2bdad103",
+    "url": "./js/vendor.bundle.js"
+  },
+  {
+    "revision": "854fe2768dea81e60035be21f5e66f2a",
+    "url": "./assets/images/icon-loading.png"
+  },
+  {
+    "revision": "36afe6abe995e6efbb97ba994da61e42",
+    "url": "./assets/images/track.png"
+  },
+  {
+    "revision": "220afd743d9e9643852e31a135a9f3ae",
+    "url": "./assets/js/jquery-3.4.1.min.js"
+  },
+  {
+    "revision": "5dc48cdd096604557ab72db557816e27",
+    "url": "./assets/images/tree3.png"
+  },
+  {
+    "revision": "ecc57f047d0d82da2c0dcdfed2a30a6d",
+    "url": "./assets/images/tree1.png"
+  },
+  {
+    "revision": "9e344fd94d51c63625e11f1442dd20de",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5606887730638d8d91617a3211859eee",
+    "url": "./assets/images/tree2.png"
+  },
+  {
+    "revision": "b767207a9d0580a2b003d50142067f7d",
+    "url": "./assets/images/goods.png"
+  },
+  {
+    "revision": "ec9546f70f04b85807d51a1f7fc5ea3e",
+    "url": "./assets/images/2.png"
+  },
+  {
+    "revision": "298c52dd5fbbf7272a0e2db71f36ef70",
+    "url": "./assets/images/3.png"
+  },
+  {
+    "revision": "f66918779e9dd236646527eff2d9b036",
+    "url": "./assets/images/cactus.png"
+  },
+  {
+    "revision": "d16ff7fdf3d97ac4f2371100a0bf3e77",
+    "url": "./assets/images/icon-charts.png"
+  },
+  {
+    "revision": "00006ded0f31bb8e3ece5f2af1cbe2d2",
+    "url": "./assets/images/go.png"
+  },
+  {
+    "revision": "f028595776e3bf5824beaecb9be63f48",
+    "url": "./assets/images/icon-shop.png"
+  },
+  {
+    "revision": "e0f60c6a53a9fa0342c28534a5aca4c9",
+    "url": "./assets/images/icon-prize.png"
+  },
+  {
+    "revision": "c91cb53937871e72735cd7a5918bccb0",
+    "url": "./assets/images/bg-game-over.png"
+  },
+  {
+    "revision": "4fe24fb1a5556559a1812942d68bac62",
+    "url": "./assets/images/img-explain.png"
+  },
+  {
+    "revision": "624227968a14f7ab9ea52e2168662d8c",
+    "url": "./assets/images/girl.png"
+  },
+  {
+    "revision": "94f1da1b4dd37a8925768674dd72d2ed",
+    "url": "./assets/images/bg-run-num.png"
+  },
+  {
+    "revision": "78d416646fc134d1f66919b6d2581170",
+    "url": "./assets/images/img-prd-num.png"
+  },
+  {
+    "revision": "55a9dc893dd6f84470933dc1e0b0a44b",
+    "url": "./assets/images/icon-close.png"
+  },
+  {
+    "revision": "460bef2c99758c2322f08d6d04482df0",
+    "url": "./assets/images/track-bg.jpg"
+  },
+  {
+    "revision": "099ca6e988ab74fb2b8f5365a4216ccf",
+    "url": "./assets/images/crash.png"
+  },
+  {
+    "revision": "8f522d5796257bd7cbea10e12c45075c",
+    "url": "./assets/images/img-run-num.png"
+  },
+  {
+    "revision": "c37fcbefea8f1f98be6d6bc09c3aea66",
+    "url": "./assets/images/1.png"
+  },
+  {
+    "revision": "194577a7e20bdcc7afbb718f502c134c",
+    "url": "./assets/images/.DS_Store"
+  },
+  {
+    "revision": "3dbfb6324411f6462c0c94191c82bdff",
+    "url": "./assets/images/+200.png"
+  },
+  {
+    "revision": "489e2ecdea323b70baf24d1e22ebb0b5",
+    "url": "./assets/images/+10.png"
+  },
+  {
+    "revision": "4e6e564fbc8946f00f9cd5d4e7ffe512",
+    "url": "./assets/css/style.css"
+  },
+  {
+    "revision": "283a59c8aa6d34e6af8d0590805fbb01",
+    "url": "./assets/.DS_Store"
+  }
+];
